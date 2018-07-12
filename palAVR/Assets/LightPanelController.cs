37b5d2f6276483d228740111e0df109c0633e27e
Unknown <ivirgili@wellesley.edu>
2018-07-12 15:01:01 +0200

    Wheel Rotation, Light Panel

---
+++ palAVR/Assets/LightPanelController.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightPanelController : MonoBehaviour {
+
+    public float lastX;
+    public float lastZ;
+    public float lastSpeed;
+    public float currentSpeed;
+    public float deltaSpeed;
+
+	// Use this for initialization
+	void Start () {
+        lastX = transform.position.x;
+        lastZ = transform.position.z;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        currentSpeed = GetDistance(transform.position.x, lastX, transform.position.z, lastZ) * 60f;
+        deltaSpeed = currentSpeed - lastSpeed;
+        Debug.Log("Delta Speed: " + deltaSpeed);
+        lastSpeed = currentSpeed;
+
+        this.LightColor();
+	}
+
+    // gets the distance between two points using x and z coords
+    public float GetDistance(float x1, float x2, float z1, float z2)
+    {
+        float deltaX = x2 - x1;
+        float deltaZ = z2 - z1;
+        return Mathf.Sqrt(deltaX * deltaX + deltaZ * deltaZ);
+    }
+
+    public void LightColor() {
+        Material panel = GetComponent<Renderer>().material;
+        Light leftLight = GameObject.Find("LeftLight").GetComponent<Light>();
+        Light rightLight = GameObject.Find("RightLight").GetComponent<Light>();
+
+        if (deltaSpeed < 0) { // if the car is slowing down
+            panel.SetColor("_EmissionColor", Color.yellow);
+            leftLight.color = Color.yellow;
+            rightLight.color = Color.yellow;
+        } if (deltaSpeed < 0.000001f && deltaSpeed > -0.000001f) { // if the car is stopped
+            panel.SetColor("_EmissionColor", Color.green);
+            leftLight.color = Color.green;
+            rightLight.color = Color.green;
+        } else {
+            panel.SetColor("_EmissionColor", Color.red);
+            leftLight.color = Color.red;
+            rightLight.color = Color.red;
+        }
+    }
+}
--- palAVR/Assets/PathManager.cs
+++ palAVR/Assets/PathManager.cs
@@ -5,28 +5,78 @@ using DG.Tweening;
 
 public class PathManager : MonoBehaviour {
     
-    public Vector3[] waypoints1 = { new Vector3(-6.921875f,1f,42.59715f), new Vector3(-4.191406f,1f,42.44843f), new Vector3(-2.089844f,1f,41.15955f), new Vector3(-1.609375f,1f,38.01234f), new Vector3(-2.068359f,1f,-42.65283f) };
-    public Vector3[] waypoints2 = { new Vector3(-1.714844f,1f,-18.45227f), new Vector3(-0.5429688f,1f,-38.03741f), new Vector3(-0.90625f,1f,-40.88074f), new Vector3(-3.402344f,1f,-42.45447f), new Vector3(-6.457031f,1f,-42.87268f), new Vector3(-23.32813f,1f,-42.41516f), new Vector3(-32.60156f,1f,-40.63574f), new Vector3(-36.08203f,1f,-39.28369f), new Vector3(-40.13672f,1f,-37.08124f), new Vector3(-43.65625f,1f,-34.58752f), new Vector3(-47.67969f,1f,-30.06952f), new Vector3(-50.73828f,1f,-25.1709f), new Vector3(-51.85156f,1f,-19.78284f), new Vector3(-52.64453f,1f,-13.20251f), new Vector3(-52.57813f,1f,13.17505f), new Vector3(-51.83594f,1f,20.64825f), new Vector3(-49.5f,1f,27.2572f), new Vector3(-44.98828f,1f,32.98553f), new Vector3(-39.89453f,1f,37.48236f), new Vector3(-33.46484f,1f,40.55725f), new Vector3(-26.43164f,1f,42.14038f), new Vector3(-17.91406f,1f,42.48737f) };
     public PathType pathType = PathType.Linear;
+    public Transform pedestrian; // needs to be SteamVR/[CameraRig], use PedPlaceholder for now
+    private float pedX;
+    private float pedZ;
+    private float stopX;
+    private float stopZ;
+    public float speed;
 
-	void Start () {
+    void Start()
+    {
+        //pedestrian = GameObject.Find("SteamVR/[CameraRig]").transform;
+        //pedPlaceholder = GameObject.Find("PedPlaceholder").transform;
+
+        // x and z coordinates of pedestrian established once in start function
+        pedX = pedestrian.position.x;
+        pedZ = pedestrian.position.z;
+        stopX = pedX + 3.5f;
+        stopZ = pedZ + 7f;
+        Vector3 stop = new Vector3(stopX, 1f, stopZ);
+        float path1distance = 65.39942f;
+        float path2distance = 233.0574f;
+        float path1time = path1distance/speed;
+        float path2time = path2distance/speed;
+
+        Vector3[] waypoints1 = {
+            new Vector3(-6.921875f,1f,42.59715f),
+            new Vector3(-4.191406f,1f,42.44843f),
+            new Vector3(-2.089844f,1f,41.15955f),
+            new Vector3(-1.609375f,1f,38.01234f),
+            stop};
+        Vector3[] waypoints2 = {
+            stop,
+            new Vector3(-0.5429688f,1f,-38.03741f),
+            new Vector3(-0.90625f,1f,-40.88074f),
+            new Vector3(-3.402344f,1f,-42.45447f),
+            new Vector3(-6.457031f,1f,-42.87268f),
+            new Vector3(-23.32813f,1f,-42.41516f),
+            new Vector3(-32.60156f,1f,-40.63574f),
+            new Vector3(-36.08203f,1f,-39.28369f),
+            new Vector3(-40.13672f,1f,-37.08124f),
+            new Vector3(-43.65625f,1f,-34.58752f),
+            new Vector3(-47.67969f,1f,-30.06952f),
+            new Vector3(-50.73828f,1f,-25.1709f),
+            new Vector3(-51.85156f,1f,-19.78284f),
+            new Vector3(-52.64453f,1f,-13.20251f),
+            new Vector3(-52.57813f,1f,13.17505f),
+            new Vector3(-51.83594f,1f,20.64825f),
+            new Vector3(-49.5f,1f,27.2572f),
+            new Vector3(-44.98828f,1f,32.98553f),
+            new Vector3(-39.89453f,1f,37.48236f),
+            new Vector3(-33.46484f,1f,40.55725f),
+            new Vector3(-26.43164f,1f,42.14038f),
+            new Vector3(-17.91406f,1f,42.48737f) };
+        
         Sequence sequence = DOTween.Sequence();
-        Tween path1 = transform.DOPath(waypoints1, 30, pathType).SetLookAt(0.01f).SetSpeedBased(true);
+        Tween path1 = transform.DOPath(waypoints1, path1time, pathType).SetLookAt(0.01f);
+        path1.SetSpeedBased(true);
         // .SetLookAt orients the transform to the path
-        Tween path2 = transform.DOPath(waypoints2, 30, pathType).SetLookAt(0.01f).SetSpeedBased(true);
+        Tween path2 = transform.DOPath(waypoints2, path2time, pathType).SetLookAt(0.01f);
+        path2.SetSpeedBased(true);
 
         sequence.Append(path1);
-
-
-
+        // if pedestrian makes stop motion, no interval should be appended
+        sequence.AppendInterval(10f);
         sequence.Append(path2);
+        sequence.SetLoops(100, LoopType.Restart);
     }
 
-
     // KNOWN ISSUES:
-    // first point of second sequence is too early
-    // last point of first path and first point of second path need
-    // to be dependent on where the hydrant/pedestrian is
+    // set ease
     //
-    // needs to be speed-based (or time needs to be calculated based on speed) (.SetSpeedBased() not working?)
+    // slow-down path between first and second with added interval for ped crossing. safe to continue when
+    // ped no longer in collider
+    // needs to loop
 }
