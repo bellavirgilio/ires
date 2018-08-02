719f52b6e1a7e113ddc83c15a230221cce2ec8ac
Unknown <ivirgili@wellesley.edu>
2018-08-02 11:51:06 +0200

    Fixed Sensor Car Light Panel

--- palAVR/Assets/Scripts/LightPanelController.cs
+++ palAVR/Assets/Scripts/LightPanelController.cs
@@ -38,10 +38,12 @@ public class LightPanelController : MonoBehaviour {
 	void Update () {
         currentSpeed = GetDistance(transform.position.x, lastX, transform.position.z, lastZ) * 60f;
         deltaSpeed = currentSpeed - lastSpeed;
-        Debug.Log("Delta Speed: " + deltaSpeed);
+        lastX = transform.position.x;
+        lastZ = transform.position.z;
+        // Debug.Log("Delta Speed: " + deltaSpeed);
         lastSpeed = currentSpeed;
 
-        this.LightColor();
+        //this.LightColor();
 	}
 
     // gets the distance between two points using x and z coords
@@ -58,12 +60,11 @@ public class LightPanelController : MonoBehaviour {
         // Light rightLight = GameObject.Find("RightLight").GetComponent<Light>();
 
         ThalmicMyo thalmicMyo = myo.GetComponent<ThalmicMyo>();
-
-        if (thalmicMyo.pose == Pose.FingersSpread)
-        {
+        //if (thalmicMyo.pose == Pose.FingersSpread)
+        if (deltaSpeed < 0) { // car is slowing down
             slowing = true;
-            //Debug.Log("Slowing begins: ");
-        } if (slowing) {
+        }
+        if (slowing) {
             panel.SetColor("_EmissionColor", Color.yellow);
             leftLight.color = Color.yellow;
             rightLight.color = Color.yellow;
@@ -74,8 +75,8 @@ public class LightPanelController : MonoBehaviour {
             panel.SetColor("_EmissionColor", Color.green);
             leftLight.color = Color.green;
             rightLight.color = Color.green;
-            //slowing = false;
-        } else if (!slowing) { // if the car is not slowing down and not stopped
+            // slowing = false;
+        } if (!slowing) { // if the car is not slowing down and not stopped
             panel.SetColor("_EmissionColor", Color.red);
             leftLight.color = Color.red;
             rightLight.color = Color.red;
--- palAVR/Assets/SensorPathManagerTest.cs
+++ palAVR/Assets/SensorPathManagerTest.cs
@@ -5,8 +5,11 @@ using DG.Tweening;
 using System.Diagnostics;
 using System;
 
-public class SensorPathManager : MonoBehaviour
+// old sensor path manager, includes stopwatch
+
+public class SensorPathManagerTest : MonoBehaviour
 {
+
     // determining the speed of the vehicle
     private float lastX;
     private float lastZ;
@@ -53,10 +56,11 @@ public class SensorPathManager : MonoBehaviour
     private Vector3 sensorStop;
     private bool sensed; // becomes true OnTriggerEnter, false OnTriggerExit
 
-    // keeps track of how many times a ped is sensed/functions that only run once
-    private int senseCount;
-    private bool oneTime;
-    private bool oneTime2;
+    // TODO: keeps track of how many times pedestrian enters collider... necessary?
+    private int stopCount;
+
+    // for timing a car loop. needed to set delays/intervals
+    private Stopwatch stopwatch = new Stopwatch();
 
     void Start()
     {
@@ -67,8 +71,9 @@ public class SensorPathManager : MonoBehaviour
         panel = GameObject.Find("LightPanel").GetComponent<Renderer>().material;
         leftLight = GameObject.Find("LeftLight").GetComponent<Light>();
         rightLight = GameObject.Find("RightLight").GetComponent<Light>();
-
-        RedLights();
+        panel.SetColor("_EmissionColor", Color.red);
+        leftLight.color = Color.red;
+        rightLight.color = Color.red;
 
         // scales km speed
         speed = kmSpeed / 3.6f;
@@ -111,6 +116,7 @@ public class SensorPathManager : MonoBehaviour
         // nonstop path
         Tween initial = transform.DOPath(initialPoints, nonstopPathTime, pathType).SetLookAt(0.01f); // nonstop tween
 
+        stopwatch.Start();
         sequence1 = DOTween.Sequence();
         sequence1.Append(initial);
     }
@@ -121,20 +127,9 @@ public class SensorPathManager : MonoBehaviour
         if (col.gameObject.tag.Equals("PedCamera"))
         {
             sensed = true;
-            senseCount++;
-            UnityEngine.Debug.Log("collider entered");
+            // UnityEngine.Debug.Log("collider entered");
         }
         // UnityEngine.Debug.Log("sensed is " + sensed);
-    }
-
-    public void OnTriggerExit(Collider col)
-    {
-        if (col.gameObject.tag.Equals("PedCamera"))
-        {
-            sensed = false;
-
-            UnityEngine.Debug.Log("collider exited");
-        }
     }
 
     void Update()
@@ -151,20 +146,18 @@ public class SensorPathManager : MonoBehaviour
 
         // UnityEngine.Debug.Log("Current speed: " + currentSpeed);
 
+        if (currentSpeed < 0.000001f && currentSpeed > -0.000001f && transformCheck > 0.0f)
+        {
+            GreenLights();
+        }
+
         // if the pedestrian is in the collider, checks the car's position
         // and creates a point at which the car starts to slow down
-        if (sensed && !oneTime)
+        if (sensed)
         {
             sensorX = this.transform.position.x;
             sensorZ = this.transform.position.z;
-            UnityEngine.Debug.Log("sensorStop: " + "(" + sensorX + ", 0.5, " + sensorZ + ")");
             sensorStop = new Vector3(sensorX, 0.5f, sensorZ);
-            oneTime = true;
-        }
-
-        if (sensed && currentSpeed < 0.000001f && currentSpeed > -0.000001f)
-        {
-            GreenLights();
         }
 
         // slowdown path
@@ -182,14 +175,28 @@ public class SensorPathManager : MonoBehaviour
             continuePoints = new[] { stop, new Vector3(2f, 0.5f, 65.3f), new Vector3(2f, 0.5f, 123.9922f), new Vector3(1.934553f, 0.5f, 126.453f), new Vector3(2.506611f, 0.5f, 128.8034f), new Vector3(4.788719f, 0.5f, 130.0849f), new Vector3(24.65626f, 0.5f, 130.2603f), new Vector3(34.32539f, 0.5f, 128.3672f), new Vector3(39.57445f, 0.5f, 125.626f), new Vector3(43.88325f, 0.5f, 122.8066f), new Vector3(48.01571f, 0.5f, 118.5967f), new Vector3(50.43028f, 0.5f, 113.5117f), new Vector3(52.45807f, 0.5f, 108.1045f), new Vector3(52.84766f, 0.5f, 85.06738f), new Vector3(53.3808f, 0.5f, -13.21484f), new Vector3(51.85156f, 0.5f, -19.78284f), new Vector3(50.73828f, 0.5f, -25.1709f), new Vector3(47.67969f, 0.5f, -30.06952f), new Vector3(43.65625f, 0.5f, -34.58752f), new Vector3(40.13672f, 0.5f, -37.08124f), new Vector3(36.08203f, 0.5f, -39.28369f), new Vector3(32.60156f, 0.5f, -40.63574f), new Vector3(23.32813f, 0.5f, -42.41516f) };
         }
 
-        if (!oneTime2 && senseCount == 1)
+        // for timekeeping
+        if (this.transform.position == new Vector3(-24.60913f, 0.5f, 129.6172f) || this.transform.position == new Vector3(23.32813f, 0.5f, -42.41516f))
         {
-            PathFind();
-            oneTime2 = true;
+            stopwatch.Stop();
+            TimeSpan ts = stopwatch.Elapsed;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+            ts.Hours, ts.Minutes, ts.Seconds,
+            ts.Milliseconds / 10);
+            //UnityEngine.Debug.Log("Car loop time: " + elapsedTime);
         }
 
-        if (!sensed && oneTime2 == true && currentSpeed > 0.0f) {
-            RedLights();
+        // control that prevents user from calling stop more than once
+        if (sensed && stopCount == 0)
+        {
+            stopCount = 1;
+            sensed = false;
+        }
+
+        if (stopCount == 1)
+        {
+            this.PathFind();
+            stopCount++;
         }
     }
 
@@ -203,6 +210,8 @@ public class SensorPathManager : MonoBehaviour
 
     public void PathFind()
     {
+        // TODO is only working for the lights on the first car....... before some changes
+
         Tween llYellowWarning = leftLight.DOColor(Color.yellow, 8f);
         Tween rlYellowWarning = rightLight.DOColor(Color.yellow, 8f);
         Tween panelYellowWarning = panel.DOColor(Color.yellow, 8f);
@@ -236,19 +245,5 @@ public class SensorPathManager : MonoBehaviour
         panel.SetColor("_EmissionColor", Color.green);
         leftLight.color = Color.green;
         rightLight.color = Color.green;
-    }
-
-    public void RedLights()
-    {
-        panel.SetColor("_EmissionColor", Color.red);
-        leftLight.color = Color.red;
-        rightLight.color = Color.red;
-    }
-
-    public void YellowLights()
-    {
-        panel.SetColor("_EmissionColor", Color.yellow);
-        leftLight.color = Color.yellow;
-        rightLight.color = Color.yellow;
     }
 }
